6c621ea03f46c7379087c787bf2a1f11e0f6f4aa
kochacha <kouki4kairi10seima9@gmail.com>
2021-12-19 21:23:56 +0900

    fix inline vector class

--- DirectX12/Vector2.h
+++ DirectX12/Vector2.h
@@ -19,13 +19,109 @@ namespace KochaEngine
 
 		Vector2& operator+=(const Vector2& v);
 		Vector2& operator-=(const Vector2& v);
-		Vector2& operator*=(float s);
-		Vector2& operator/=(float s);
+		Vector2& operator*=(const float s);
+		Vector2& operator/=(const float s);
 	};
 }
 
-const KochaEngine::Vector2 operator+(const KochaEngine::Vector2& v1, const KochaEngine::Vector2& v2);
-const KochaEngine::Vector2 operator-(const KochaEngine::Vector2& v1, const KochaEngine::Vector2& v2);
-const KochaEngine::Vector2 operator*(const KochaEngine::Vector2& v, float s);
-const KochaEngine::Vector2 operator*(float s, const KochaEngine::Vector2& v);
-const KochaEngine::Vector2 operator/(const KochaEngine::Vector2& v, float s);
+//const KochaEngine::Vector2 operator+(const KochaEngine::Vector2& v1, const KochaEngine::Vector2& v2);
+//const KochaEngine::Vector2 operator-(const KochaEngine::Vector2& v1, const KochaEngine::Vector2& v2);
+//const KochaEngine::Vector2 operator*(const KochaEngine::Vector2& v, float s);
+//const KochaEngine::Vector2 operator*(float s, const KochaEngine::Vector2& v);
+//const KochaEngine::Vector2 operator/(const KochaEngine::Vector2& v, float s);
+
+
+inline KochaEngine::Vector2::Vector2() : Vector2(0, 0)
+{
+}
+
+inline KochaEngine::Vector2::Vector2(float x, float y) : XMFLOAT2(x, y)
+{
+}
+
+inline float KochaEngine::Vector2::length() const
+{
+	return sqrt(x * x + y * y);
+}
+
+inline KochaEngine::Vector2& KochaEngine::Vector2::normalize()
+{
+	float len = length();
+	if (len != 0)
+	{
+		return *this /= len;
+	}
+	return *this;
+}
+
+inline float KochaEngine::Vector2::dot(const Vector2& v) const
+{
+	return x * v.x + y * v.y;
+}
+
+inline KochaEngine::Vector2 KochaEngine::Vector2::operator+() const
+{
+	return *this;
+}
+
+inline KochaEngine::Vector2 KochaEngine::Vector2::operator-() const
+{
+	return Vector2(-x, -y);
+}
+
+inline KochaEngine::Vector2& KochaEngine::Vector2::operator+=(const Vector2& v)
+{
+	x += v.x;
+	y += v.y;
+	return *this;
+}
+
+inline KochaEngine::Vector2& KochaEngine::Vector2::operator-=(const Vector2& v)
+{
+	x -= v.x;
+	y -= v.y;
+	return *this;
+}
+
+inline KochaEngine::Vector2& KochaEngine::Vector2::operator*=(const float s)
+{
+	x *= s;
+	y *= s;
+	return *this;
+}
+
+inline KochaEngine::Vector2& KochaEngine::Vector2::operator/=(const float s)
+{
+	x /= s;
+	y /= s;
+	return *this;
+}
+
+inline const KochaEngine::Vector2 operator+(const KochaEngine::Vector2& v1, const KochaEngine::Vector2& v2)
+{
+	KochaEngine::Vector2 temp(v1);
+	return temp += v2;
+}
+
+inline const KochaEngine::Vector2 operator-(const KochaEngine::Vector2& v1, const KochaEngine::Vector2& v2)
+{
+	KochaEngine::Vector2 temp(v1);
+	return temp -= v2;
+}
+
+inline const KochaEngine::Vector2 operator*(const KochaEngine::Vector2& v, float s)
+{
+	KochaEngine::Vector2 temp(v);
+	return temp *= s;
+}
+
+inline const KochaEngine::Vector2 operator*(float s, const KochaEngine::Vector2& v)
+{
+	return v * s;
+}
+
+inline const KochaEngine::Vector2 operator/(const KochaEngine::Vector2& v, float s)
+{
+	KochaEngine::Vector2 temp(v);
+	return temp /= s;
+}
--- DirectX12/Vector4.h
+++ DirectX12/Vector4.h
@@ -17,13 +17,104 @@ namespace KochaEngine
 
 		Vector4& operator+=(const Vector4& v);
 		Vector4& operator-=(const Vector4& v);
-		Vector4& operator*=(float s);
-		Vector4& operator/=(float s);
+		Vector4& operator*=(const float s);
+		Vector4& operator/=(const float s);
 	};
 }
 
-const KochaEngine::Vector4 operator+(const KochaEngine::Vector4& v1, const KochaEngine::Vector4& v2);
-const KochaEngine::Vector4 operator-(const KochaEngine::Vector4& v1, const KochaEngine::Vector4& v2);
-const KochaEngine::Vector4 operator*(const KochaEngine::Vector4& v, float s);
-const KochaEngine::Vector4 operator*(float s, const KochaEngine::Vector4& v);
-const KochaEngine::Vector4 operator/(const KochaEngine::Vector4& v, float s);
+//const KochaEngine::Vector4 operator+(const KochaEngine::Vector4& v1, const KochaEngine::Vector4& v2);
+//const KochaEngine::Vector4 operator-(const KochaEngine::Vector4& v1, const KochaEngine::Vector4& v2);
+//const KochaEngine::Vector4 operator*(const KochaEngine::Vector4& v, float s);
+//const KochaEngine::Vector4 operator*(float s, const KochaEngine::Vector4& v);
+//const KochaEngine::Vector4 operator/(const KochaEngine::Vector4& v, float s);
+
+
+inline KochaEngine::Vector4::Vector4()
+	:Vector4(0, 0, 0, 0)
+{
+}
+
+inline KochaEngine::Vector4::Vector4(float x, float y, float z, float w)
+	: XMFLOAT4(x, y, z, w)
+{
+}
+
+inline void KochaEngine::Vector4::Zero()
+{
+	x = y = z = w = 0;
+}
+
+inline KochaEngine::Vector4 KochaEngine::Vector4::operator+() const
+{
+	return *this;
+}
+
+inline KochaEngine::Vector4 KochaEngine::Vector4::operator-() const
+{
+	return Vector4(-x, -y, -z, -w);
+}
+
+inline KochaEngine::Vector4& KochaEngine::Vector4::operator+=(const Vector4& v)
+{
+	x += v.x;
+	y += v.y;
+	z += v.z;
+	w += v.w;
+	return *this;
+}
+
+inline KochaEngine::Vector4& KochaEngine::Vector4::operator-=(const Vector4& v)
+{
+	x -= v.x;
+	y -= v.y;
+	z -= v.z;
+	w -= v.w;
+	return *this;
+}
+
+inline KochaEngine::Vector4& KochaEngine::Vector4::operator*=(const float s)
+{
+	x *= s;
+	y *= s;
+	z *= s;
+	w *= s;
+	return *this;
+}
+
+inline KochaEngine::Vector4& KochaEngine::Vector4::operator/=(const float s)
+{
+	x /= s;
+	y /= s;
+	z /= s;
+	w /= s;
+	return *this;
+}
+
+inline const KochaEngine::Vector4 operator+(const KochaEngine::Vector4& v1, const KochaEngine::Vector4& v2)
+{
+	KochaEngine::Vector4 temp(v1);
+	return temp += v2;
+}
+
+inline const KochaEngine::Vector4 operator-(const KochaEngine::Vector4& v1, const KochaEngine::Vector4& v2)
+{
+	KochaEngine::Vector4 temp(v1);
+	return temp -= v2;
+}
+
+inline const KochaEngine::Vector4 operator*(const KochaEngine::Vector4& v, float s)
+{
+	KochaEngine::Vector4 temp(v);
+	return temp *= s;
+}
+
+inline const KochaEngine::Vector4 operator*(float s, const KochaEngine::Vector4& v)
+{
+	return v * s;
+}
+
+inline const KochaEngine::Vector4 operator/(const KochaEngine::Vector4& v, float s)
+{
+	KochaEngine::Vector4 temp(v);
+	return temp /= s;
+}
